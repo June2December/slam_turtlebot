9c8eab11f3bb406a77c686b99f41f9e79cacea1c
MARKCH <cocoffeechan09@gmail.com>
2026-03-12 21:57:31 +0900

    detect

--- src/detection/setup.py
+++ src/detection/setup.py
@@ -30,6 +30,7 @@ setup(
             'amr2_detect = detection.obj_det_amr2:main',
             'amr2_detect_unkonwn = detection.obj_det_depth_unknown:main',
             'amr1_detect_test1 = detection.depth_aligned_dual:main',
+            'amr1_detect_test2 = detection.amr1_observe_v2_detect_loop:main'
         ],
     },
 )
